097d5c4ab2d94fc652d7ab74123099cf3958c366
常华锋 <wb-chf309549@alibaba-inc.com>
2017-09-22 18:06:35 +0800

    修改

--- src/main/java/com/changhf/schedule/QuartzJob.java
+++ src/main/java/com/changhf/schedule/QuartzJob.java
@@ -1,30 +1,33 @@
 package com.changhf.schedule;
 
-import java.util.Date;
-
+import com.changhf.utils.DateConverter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 @Component
-@EnableScheduling
+// @EnableScheduling
 public class QuartzJob {
+    @Autowired
+    private DateConverter dateConverter;
 	private static final Logger logger = LoggerFactory.getLogger(QuartzJob.class);
 
-	// @Scheduled(cron = "0 */5 * * * ?")//每小时执行一次
+	// @Scheduled(cron = "0 */5 * * * ?")//每隔五分钟执行一次
 	// @Scheduled(cron = "0 */30 * * * ?")//每隔30分
-	// @ Scheduled(cron = "*/20 * * * * ?")//每20分钟执行一次
+	// @ Scheduled(cron = "*/20 * * * * ?")//每20秒执行一次
 	// @Scheduled(cron = "0 0 7 * * ?")//每天凌晨7点整
 	// @Scheduled(cron = "0 30 14 * * ?")//每天14:30
     
     /**
      * 测试
      */
-    @Scheduled(cron = "0 30 10 * * ?")//每天早上10点半
+    @Scheduled(cron = "0 */2 * * * ?")//每隔两分钟
     public void printCurrentTime(){
         logger.info("定时器printCurrentTime------------start");
-        System.out.println(new Date());
+        System.out.println(dateConverter.format(new Date()));
         logger.info("定时器printCurrentTime------------end");
     }
 }
